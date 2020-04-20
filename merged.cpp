b61a02e8095f10048479b01310ba04959eefb26a
ScotchGeek <62902884+ScotchGeek@users.noreply.github.com>
2020-04-20 20:41:37 +0530

    Update merged.cpp

--- merged.cpp
+++ merged.cpp
@@ -1,9 +1,13 @@
+//Including necessary libraries
 #include<iostream>
 #include <vector>
 #include <limits.h>
 #include<math.h>
 using namespace std;
 
+
+//Declaration and initialization of variables
+//Some variables initialized to default values
 vector<vector<int>> cit(8);
 vector<int> packetdelay(8);
 int N=8,b=4;
@@ -17,7 +21,7 @@ int smallest_index,value_of_smallestl_index;
 vector<vector<int>> oppacket(8);
 vector<int> drops;
 
-
+//Function for calculating average delay
 void calculate_average_delay()
 {
     for(i=0;i<packetdelay.size();i++)
@@ -27,34 +31,40 @@ void calculate_average_delay()
     printf("%lf ",aver_delay);
 }
 
-
+//Function for standard deviation calculation.
 void calculate_stddevation_delay()
 {
     int n=packetdelay.size();
     for(i=0;i<n;i++)
         sd+=pow((packetdelay[i]-aver_delay),2);
+    //Calculate difference over all means 
     sd=sd/n;
+    //Find mean of the above
     sd=sqrt(aver_delay);
+    //Square root of given difference
     printf("%lf ",sd);
 }
 
-
+//Method to Calculate how much link utilization is there
 void calculate_link_utilization()
 {
     double link=0;
     for(i=0;i<packet.size();i++)
             link+=totalLinksUsed[i];
+    //Iterate over all slots
     link=link/(packet.size()*maxtimeslots);
     printf("%lf ",link);
 }
 
+
+//Traffic generation for INQ
 void traffic_generation_one()
 {
    
         for(j=0;j<N;j++){
-           if(((double) rand() / (RAND_MAX)) < packetgenprob)
-           {
-               if((packet[j].size() == b))
+           if(((double) rand() / (RAND_MAX)) < packetgenprob)               //If generated random number is less than entered packet generation probability
+           {                                                                // generate the packet        
+               if((packet[j].size() == b))              //If all input buffers are full.. go on with next port
                 continue;
                 else
                {
@@ -67,20 +77,21 @@ void traffic_generation_one()
         }
 }
 
+//Traffic generation for KOUQ
 void traffic_generation_two()
 {
     drop=0;
-    for(j=0;j<N;j++)
+    for(j=0;j<N;j++)        //Initialize all dropped packets to -1
         drops[i]=-1;
     for(j=0;j<N;j++){
-        int port=(rand() % (N));
+        int port=(rand() % (N));        //Generate random port
        if(((double) rand() / (RAND_MAX)) < packetgenprob)
        {
             if((packet[port].size() == K))
             {
-                drops[port]=0;
-                drop++;
-                continue;
+                drops[port]=0;      //If output port has more than or equal to
+                drop++;             //dropout probability increment drop and continue
+                continue;           //with next port.
             }
             else
             {
@@ -94,15 +105,15 @@ void traffic_generation_two()
     
     for(j=0;j<N;j++)
         if(drops[i] == 0)
-            drop++;
+            drop++;         //Calculate drop frequency
     tot=(long double)(drop/N);
     dt+=tot;
 }
 
-
+////Traffic generation for IsLIP
 void traffic_generation_three()
 {
-   
+    //Iterate over all ports  
         for(j=0;j<N;j++){
            if(((double) rand() / (RAND_MAX)) < packetgenprob)
            {
@@ -119,46 +130,50 @@ void traffic_generation_three()
         }
 }
 
+//Function for updation of links
 void initialize_totalLinksUsed()
 {
     for(i=0;i<N;i++)
         totalLinksUsed[i]=0;
 }
 
+//Method for port updation 
 void initialize_portInUse()
 {
     for(i=0;i<N;i++)
         portInUse[i]=0;
 }
 
+//Scheduling INQ
 void one_schedule()
 {
     initialize_portInUse();
     for(i=0;i<N;i++)
     {
         
-        if(packet[i].size() != 0)
+        if(packet[i].size() != 0)       //If there exists a packet generated at given port
         {
             k=packet[i][0];
             if((portInUse[k]) == 0)   
-            {    packetdelay.push_back(time1-cit[i][0]+1);
+            {    packetdelay.push_back(time1-cit[i][0]+1);      //Used for delay modification.
                
-            portInUse[k]=1;
-            totalLinksUsed[k]+=1;
-            cit[i].erase(cit[i].begin());
-            packet[i].erase(packet[i].begin());
+            portInUse[k]=1;                         //Indicates selected port is used.
+            totalLinksUsed[k]+=1;                   //UPdates used ports
+            cit[i].erase(cit[i].begin());           //Remove entry from arrival time queue
+            packet[i].erase(packet[i].begin());     //Remove entry from detination port queue
             }
         }
         
     }
 }
 
-
+//Calculate drop frequency
 void print_drop_frequency()
 {
     printf("%Lf ",(long double)(dt/(maxtimeslots)));    
 }
 
+//Scheduling KOUQ
 void two_schedule()
 {
     initialize_portInUse();
@@ -181,7 +196,7 @@ void two_schedule()
     }
 }
 
-
+//IsLIP Scheduling
 void three_schedule()
 {
     initialize_portInUse();
@@ -190,19 +205,19 @@ void three_schedule()
         
         if(packet[i].size() != 0)
         {
-            smallest_index=-1,value_of_smallestl_index=INT_MAX;
+            smallest_index=-1,value_of_smallestl_index=INT_MAX;     //obtaining Smallest index 
             k=packet[i][0];
-            for(j=0;j<packet[i].size();j++)
+            for(j=0;j<packet[i].size();j++)     //Iterate over output queue of selected port 
             {
             if((portInUse[k]) == 0)   
-                {    
+                {                       //If port is free
                     smallest_index=j;
                     value_of_smallestl_index=packet[i][j];
                 }
             }
-            if(smallest_index == -1)
+            if(smallest_index == -1)    //No smallest index found 
                 continue;
-            packetdelay.push_back(time1-cit[i][smallest_index]+1);
+            packetdelay.push_back(time1-cit[i][smallest_index]+1);  //Updation of all variables like KOUQ,INQ Scheduling
             portInUse[value_of_smallestl_index]=1;
             totalLinksUsed[value_of_smallestl_index]+=1;
             cit[i].erase(cit[i].begin()+smallest_index);
@@ -212,8 +227,10 @@ void three_schedule()
     }
 }
 
+//Driver function
 int main()
 {
+    //obtaining Input 
     printf("Enter number of ports:\n");
     scanf("%d",&N);
     printf("Enter number of buffers:\n");
@@ -224,13 +241,14 @@ int main()
     scanf("%Lf",&maxtimeslots);
     printf("Type 1 for INQ,2 for KOUQ,3 for IsLIP:\n");
     scanf("%d",&queue);
+    //Dropout factor entry if KOUQ selected
     if(queue == 2)
     {
         printf("Enter dropout factor:\n");
         scanf("%lf",&dropout_factor);
     }    
         
-
+    //Size adjustment of all the vectors
     initialize_totalLinksUsed();
     packet.resize(N);
     cit.resize(N);
@@ -239,6 +257,7 @@ int main()
     drops.resize(N);
     oppacket.resize(N);
     
+    //For INQ
     if(queue == 1)
     {
         printf("%d %lf ",N,packetgenprob);
@@ -253,7 +272,7 @@ int main()
         calculate_stddevation_delay();
         calculate_link_utilization();
     }
-    
+    //For KOUQ
     if(queue == 2)
     {
         printf("%d %lf ",N,packetgenprob);
@@ -270,7 +289,7 @@ int main()
         calculate_link_utilization();
         print_drop_frequency();
     }
-    
+    //For iSLIP
     if(queue == 3)
     {
         printf("%d %lf ",N,packetgenprob);
